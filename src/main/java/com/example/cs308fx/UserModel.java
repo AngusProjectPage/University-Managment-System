5e4f321a9d42fcf4d8bdb19319b10a6641c86b35
Callum Sergeant <callum@ondaweb.co.uk>
2023-11-23 17:22:15 +0000

    Working manager!

--- src/main/java/com/example/cs308fx/UserModel.java
+++ src/main/java/com/example/cs308fx/UserModel.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -154,6 +155,70 @@ public class UserModel {
             }
         }
     }
+
+    public List<String> getUnapprovedStudents() throws SQLException {
+        List<String> students = new ArrayList<>();
+        String query = "SELECT studentId, firstname, surname FROM student WHERE approved = false";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()) {
+            String studentInfo = rs.getInt("studentId") + " - " + rs.getString("firstname") + " " + rs.getString("surname");
+            students.add(studentInfo);
+        }
+
+        return students;
+    }
+
+    public void approveStudent(int studentId) throws SQLException {
+        String query = "UPDATE student SET approved = true WHERE studentId = ?";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setInt(1, studentId);
+        ps.executeUpdate();
+    }
+
+    public void addCourse(String id, String name, String description) throws SQLException {
+        String query = "INSERT INTO course (courseId, courseName, courseDescription) VALUES (?, ?, ?)";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1, id);
+        ps.setString(2, name);
+        ps.setString(3, description);
+        ps.executeUpdate();
+    }
+
+    public void addModule(String id, String name, String credit) throws SQLException {
+        String query = "INSERT INTO module (moduleId, moduleName, credit) VALUES (?, ?, ?)";
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setString(1, id);
+        ps.setString(2, name);
+        ps.setString(3, credit);
+        ps.executeUpdate();
+    }
+
+    public void updatePassword(String userId, String newPassword) throws SQLException {
+        String table;
+        if (userId.startsWith("stu")) {
+            table = "student";
+        } else if (userId.startsWith("mgr")) {
+            table = "manager";
+        } else if (userId.startsWith("lct")) {
+            table = "lecturer";
+        } else {
+            throw new IllegalArgumentException("Invalid user ID prefix.");
+        }
+
+        String query = "UPDATE " + table + " SET password = ? WHERE username = ?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, newPassword);
+            ps.setString(2, userId);
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Updating password failed, no rows affected.");
+            }
+        }
+    }
+
+
 
 
     public int getId() {
