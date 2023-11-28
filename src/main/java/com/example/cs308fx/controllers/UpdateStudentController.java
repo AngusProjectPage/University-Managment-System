7b487d43e1bc142bc616106edde469e9efaf2539
Angus Duffy CS2021 <angus.duffy.2021@uni.strath.ac.uk>
2023-11-28 20:46:50 +0000

    UI nearly complete

--- src/main/java/com/example/cs308fx/controllers/UpdateStudentController.java
+++ src/main/java/com/example/cs308fx/controllers/UpdateStudentController.java
@@ -2,14 +2,35 @@ package com.example.cs308fx.controllers;
 
 import com.example.cs308fx.Lecturer;
 import com.example.cs308fx.Student;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class UpdateStudentController {
 
     Lecturer loggedInLecturer;
     Student student;
 
+    @FXML
+    Label studentNameLabel;
+
     public void init(Lecturer loggedInLecturer, Student student) {
         this.loggedInLecturer = loggedInLecturer;
         this.student = student;
+        setStudentNameLabel();
     }
+
+    public void setStudentNameLabel() {
+        studentNameLabel.setText("Altering " + student.getFirstName() + " "  + student.getSurname() + " grades");
+    }
+
+    public void updateExamMark(ActionEvent event) {
+
+    }
+
+    public void updateLabMark(ActionEvent event) {
+
+    }
+
+
 }
